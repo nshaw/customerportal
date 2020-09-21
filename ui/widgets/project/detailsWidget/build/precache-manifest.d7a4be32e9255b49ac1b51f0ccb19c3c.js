0b5e1e54b00ade86bb575146156ae686a1c1d5a3
Nathan Shaw <n.shaw@entando.com>
2020-09-21 09:19:29 -0400

    Add widget files as well

---
+++ ui/widgets/project/detailsWidget/build/precache-manifest.d7a4be32e9255b49ac1b51f0ccb19c3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ec359990c67514b673b49ad3e63955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab7283fd1eddafd14d04",
+    "url": "/static/css/main.552b72a3.chunk.css"
+  },
+  {
+    "revision": "a348f492a3bfed941f8b",
+    "url": "/static/js/2.bc707cfa.chunk.js"
+  },
+  {
+    "revision": "ab7283fd1eddafd14d04",
+    "url": "/static/js/main.15481cee.chunk.js"
+  },
+  {
+    "revision": "3502069ec05b5d73d7b3",
+    "url": "/static/js/runtime~main.feefe0b0.js"
+  }
+]);
